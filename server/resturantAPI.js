997a4f5bd831e49f94ddf79cf153e2b709e0cd5f
UI UX 2016 <UI UX 2016>
2016-06-20 11:59:57 +0100

    server Api fixed with new menu and directory

--- server/resturantAPI.js
+++ server/resturantAPI.js
@@ -5,22 +5,15 @@ var app = express();
 app.use(bodyParser.json());
 
 
-app.get('/restaurant', function (req, res) {
-    fs.readFile( __dirname + "/" + "burgerRestaurantMenu.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
-    });
-})
-
 app.get('/restaurantMenu', function (req, res) {
-    fs.readFile( __dirname + "/" + "burgerRestaurantMenu.json", 'utf8', function (err, data) {
+    fs.readFile( "../thaiRestaurantMenu.json", 'utf8', function (err, data) {
         data = JSON.parse(data);
         res.end( JSON.stringify(data.RestaurantMenuCategories) );
     });
 })
 
 app.get('/submitOrder/:typeId/:burgerId', function (req, res) {
-    fs.readFile( __dirname + "/" + "burgerRestaurantMenu.json", 'utf8', function (err, data) {
+    fs.readFile( "../thaiRestaurantMenu.json", 'utf8', function (err, data) {
         var idOfFoodType = parseInt(req.params.typeId);
         var idOfBurgerType = parseInt(req.params.burgerId);
         var output = 'Not Found'
@@ -43,48 +36,6 @@ app.get('/submitOrder/:typeId/:burgerId', function (req, res) {
 
         res.end( JSON.stringify(output) );
     });
-})
-
-
-app.post('/albums', function (req, res) {
-    // parses the request url
-
-
-    var newAlbum = req.body;
-    fs.readFile( __dirname + "/" + "albums.json", 'utf8', 		function (err, data) {
-        data = JSON.parse( data );
-        data.push(newAlbum);
-        res.end( JSON.stringify(data));
-        fs.writeFile(__dirname + "/" + "albums.json",
-            JSON.stringify(data), function
-                (err) {
-                if (err)
-                    return console.log(err);
-            });
-    });
-
-});
-
-app.put('/albums', function (req, res) {
-    var updatedAlbum = req.body;
-    fs.readFile( __dirname + "/" + "albums.json", 'utf8',
-        function (err, data) {
-            data = JSON.parse( data );
-            for (var i = 0; i<data.length; i++) {
-                if (data[i].id == updatedAlbum.id) {
-                    data[i] = updatedAlbum;
-                }
-            }
-            res.end( JSON.stringify(data));
-            fs.writeFile(__dirname + "/" + "albums.json",
-                JSON.stringify(data), function
-                    (err) {
-                    if (err)
-                        return console.log(err);
-                });
-        });
-    res.end( "album updated if it existed" );
-
 });
 
 
