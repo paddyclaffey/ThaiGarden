40be7737d2faea1af1550ab7bac9185c451bf0c3
UI UX 2016 <UI UX 2016>
2016-06-22 16:33:49 +0100

    styling for main menu page

--- menuPage/populateMenu.js
+++ menuPage/populateMenu.js
@@ -3,8 +3,8 @@
  */
 var checkout = {
     coupon: {},
-    restaurantId : 1,
-    order :[
+    restaurantId: 1,
+    order: [
         {
             orderNum: '101',
             extras: {},
@@ -12,67 +12,65 @@ var checkout = {
         },
         {
             orderNum: '102',
-            extras: {
-                
-            },
+            extras: {},
             quantity: 1
         }
     ]
 };
 
-var ready = function() {
+var ready = function () {
     // fetches and promises
-    fetch('http://localhost:8081/restaurantMenu').then(function(result) {
+    fetch('http://localhost:8081/restaurantMenu').then(function (result) {
         return result.json(); // a promise of JSON
-    }).then(function(menu) { // the menu is now the JSON
+    }).then(function (menu) { // the menu is now the JSON
         var starterMenu = document.getElementsByClassName("menu_container");
         var counter = 0;
 
-        for(var i=0;i<menu.length; i++){
+        for (var i = 0; i < menu.length; i++) {
             starterMenu[0].innerHTML += '<div class="menu_type"></div>';
         }
 
-        for(var i=0;i<menu.length; i++) {
+        for (var i = 0; i < menu.length; i++) {
             var menu_type = document.getElementsByClassName("menu_type");
 
-            if(menu[i].Title) {
+            if (menu[i].Title) {
                 menu_type[i].innerHTML += '<div class="menu_header"' +
                     'onclick="expandCollapse(\'' + menu[i].Title + '\')"><span>' + menu[i].Title +
                     '</span><img name="' + menu[i].Title + 'expandIcon" src="../images/chevron.png"></div>' +
                     '<div class="bottom_border"></div>';
             }
 
-            if(menu[i].Items.length>0){
+            if (menu[i].Items.length > 0) {
                 for (var j = 0; j < menu[i].Items.length; j++) {
                     menu_type[i].innerHTML += '<div name="' + menu[i].Title + '" class="menu_option"></div>';
-                    menu_type[i].innerHTML += '<div id="chooseItem' + (j+counter) + '" class="menu_option_chooseItem"></div>';
+                    menu_type[i].innerHTML += '<div id="chooseItem' + (j + counter) + '" class="menu_option_chooseItem"></div>';
                 }
 
                 for (var j = 0; j < menu[i].Items.length; j++) {
                     var menu_option = document.getElementsByClassName("menu_option");
                     menu_option[counter].innerHTML += '<div class="menu_option_text">' +
-                        '<p class="menu_option_name" onclick="displayChooseItemMenu(' + counter  + ')">'  +
-                            menu[i].Items[j].Title + '</p>' +
-                        '<p class="menu_option_description">' + menu[i].Items[j].Description + '</p><hr>' +
+                        '<p class="menu_option_name" onclick="displayChooseItemMenu(' + counter + ')">' +
+                        menu[i].Items[j].Title + '</p>' +
+                        '<p class="menu_option_description">' + menu[i].Items[j].Description + '</p><hr id="line">' +
                         '</div>' +
                         '<div class="menu_option_nonText">' +
                         '<p class="menu_option_price">€' + menu[i].Items[j].Price + '</p>' +
                         '</div>';
-                    counter ++;
+                    counter++;
                 }
             }
         }
         populateCheckout();
     });
 
-    var populateCheckout = function(){
+    var populateCheckout = function () {
         //Showing all the orders
-        for(var i = 0; i<checkout.order.length; i++){
+        for (var i = 0; i < checkout.order.length; i++) {
             var orderList = document.getElementsByClassName('order_list');
             orderList[0].innerHTML += '<div class="order_list_item"></div>';
         }
-        
-        for(var i = 0; i<checkout.order.length; i++){
+
+        for (var i = 0; i < checkout.order.length; i++) {
             var orderListItem = document.getElementsByClassName('order_list_item');
             orderListItem[i].innerHTML += '<div class="menu_option_text">' +
                 '<p class="menu_option_name">' + 'Chicken & Cashew Nuts' + '</p>' +
@@ -86,50 +84,50 @@ var ready = function() {
     }
 };
 
-var expandCollapse = function (elementName){
-    var image = document.getElementsByName( elementName +'expandIcon');
-    if(!(image[0].style.transform === 'rotate(0deg)')){
+var expandCollapse = function (elementName) {
+    var image = document.getElementsByName(elementName + 'expandIcon');
+    if (!(image[0].style.transform === 'rotate(0deg)')) {
         image[0].style.transform = 'rotate(0deg)';
     }
-    else{
+    else {
         image[0].style.transform = 'rotate(90deg)';
     }
 
     var elements = document.getElementsByName(elementName);
-    for(var i=0; i<elements.length;i++){
-        if(elements[i].style.display === "none"){
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].style.display === "none") {
             elements[i].style.display = "flex";
         }
-        else{
+        else {
             elements[i].style.display = "none";
         }
     }
 }
 
-var displayCheckout = function(){
+var displayCheckout = function () {
     document.getElementById("checkout").style.display = 'flex';
     document.getElementById("proceedToCheckoutButton").style.display = 'none';
 }
 
-var confirmOrder = function(){
+var confirmOrder = function () {
     alert("confirmed");
 }
 
-var cancelOrder = function(){
+var cancelOrder = function () {
     document.getElementById("checkout").style.display = 'none';
     document.getElementById("proceedToCheckoutButton").style.display = 'block';
 }
 
-var displayChooseItemMenu = function(itemId, foodItem){
-    if(document.getElementById("chooseItem" + itemId).innerHTML === '') {
-        $.get("../chooseOrderItem/addItem.html", function(data) {
+var displayChooseItemMenu = function (itemId, foodItem) {
+    if (document.getElementById("chooseItem" + itemId).innerHTML === '') {
+        $.get("../chooseOrderItem/addItem.html", function (data) {
             var menu_type = document.getElementsByClassName("menu_option_chooseItem");
             menu_type[itemId].innerHTML += data;
         });
     }
 }
 
-var removeChooseItemMenu = function(self){
+var removeChooseItemMenu = function (self) {
     var itemId = $(self).parent().parent().parent().parent().attr('id');
     document.getElementById(itemId).innerHTML = '';
     console.log(itemId);
